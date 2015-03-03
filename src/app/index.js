b169c2a1ce50176beff1e4d9f3c0237f8ec5caf2
Lt <trepafi@gmail.com>
2015-03-03 21:23:31 +0100

    Upting app

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,17 @@
 'use strict';
 
 angular.module('ryanair', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap'])
-  .config(function ($locationProvider, $stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainCtrl'
-      });
+    .config(function ($locationProvider, $stateProvider, $urlRouterProvider) {
+        $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'app/main/main.html',
+            controller: 'MainCtrl'
+        });
 
-    $urlRouterProvider.otherwise('/');
-    $locationProvider.html5Mode(true);
-  })
-;
+        $urlRouterProvider.otherwise('/');
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    });
